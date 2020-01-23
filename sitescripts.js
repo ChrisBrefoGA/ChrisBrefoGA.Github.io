14060caecd395b76ceee8196c6d2b7b863f5808e
Christopher Brefo <christopherbrefo@yahoo.com>
2020-01-23 11:29:16 -0500

    responsive testing

--- sitescripts.js
+++ sitescripts.js
@@ -3,18 +3,10 @@ function show()
 	document.getElementById("About").style.display = "block";
 	document.getElementById("sendUp").style.display = "block";
 }
-function show2() 
-{
-	document.getElementById("Projects").style.display = "block";
-	document.getElementById("sendUp").style.display = "block";
-}
+
 function hide() 
 {
-	var tab1 = document.getElementById("Projects");
 	var tab2 = document.getElementById("About");
-	if(tab1.style.display == "block"){
-		document.getElementById("Projects").style.display = "none";	
-	}
 	if(tab2.style.display == "block"){
 		document.getElementById("About").style.display = "none";
 	}
