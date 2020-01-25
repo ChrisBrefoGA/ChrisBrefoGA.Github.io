64e994f0d96ad8ac9e2208ba4ce7a9d1efaf6eb7
Christopher Brefo <christopherbrefo@yahoo.com>
2020-01-25 13:16:35 -0500

    final touches

--- sitescripts.js
+++ sitescripts.js
@@ -3,7 +3,6 @@ function show()
 	document.getElementById("About").style.display = "block";
 	document.getElementById("sendUp").style.display = "block";
 }
-
 function hide() 
 {
 	var tab2 = document.getElementById("About");
